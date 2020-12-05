e38fd7e6ce2f4881fa074aa5c07e4cad6bec436f
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-04 21:46:45 -0500

    Updated project page to render each project with a compenent

---
+++ src/Components/Project/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+    render() {
+        return (
+            <article className="col-md-4">
+                <a href={this.props.github}
+                    target="_blank"
+                    rel="noreferrer">
+
+                    <img src={this.props.src}
+                        className="img-fluid portfolio-images"
+                        alt={this.props.name} />
+                </a>
+                <div className="row mt-3">
+                    <div className="col">
+                        <h2>{this.props.name}</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col  mb-4">
+                        <p>{this.props.description}</p>
+                        <a href={this.props.github}>Band Tribute Page</a>
+                    </div>
+                </div>
+            </article>
+        );
+    }
+};
+
+export default Project
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -8,8 +8,65 @@ import CODEQUIZ from '../images/Screen Shot 2020-10-19 at 5.24.58 PM.png';
 import WEATHERDASHBOARD from '../images/Screen Shot 2020-10-19 at 5.30.07 PM.png';
 import EMPLOYEESUMMARY from '../images/Screen Shot 2020-12-02 at 3.22.11 PM.png';
 import BROWNBAG from '../images/Screen Shot 2020-12-02 at 3.30.59 PM copy.png';
+import Project from "../Components/Project"
 
 const Projects = () => {
+    const projects = [
+        {
+            name: "Tribute Page",
+            src: TRIBUTE,
+            description: "Band tribute page built for bootcamp prework.",
+            github: "https://elliott-andrew.github.io/tribute-page/"
+        },
+        {
+            name: "Horiseon Refactoring",
+            src: HORSEON,
+            description: "Refactored a marketing agency website.",
+            github: "https://elliott-andrew.github.io/Code-Refractor/"
+        },
+        {
+            name: "Introduction Page",
+            src: INTRO_PAGE,
+            description: "Introduction page built for bootcamp prework.",
+            github: "https://elliott-andrew.github.io/prework-about-me/"
+        },
+        {
+            name: "Google Homepage",
+            src: GOOGLE_HOMEPAGE,
+            description: "Rebuild of the Google homepage.",
+            github: "https://elliott-andrew.github.io/google-homepage/"
+        },
+        {
+            name: "Code Quiz",
+            src: CODEQUIZ,
+            description: "Code quiz exercise.",
+            github: "https://github.com/elliott-andrew/Code-Quiz"
+        },
+        {
+            name: ".chopCheese",
+            src: CHOPCHEESE,
+            description: "Content aggregation app.",
+            github: "https://mikevitelli.github.io/chopCheese/"
+        },
+        {
+            name: "Weather Dashboard",
+            src: WEATHERDASHBOARD,
+            description: "Weather forecasting app.",
+            github: "https://github.com/elliott-andrew/weather-dashboard"
+        },
+        {
+            name: "Employee Tracker",
+            src: EMPLOYEESUMMARY,
+            description: "Easily organize your employees.",
+            github: "https://github.com/elliott-andrew/Employee-Tracker"
+        },
+        {
+            name: "Brown Bag",
+            src: BROWNBAG,
+            description: "An online ordering app for local restaurants.",
+            github: "https://foodappbcs.herokuapp.com/"
+        },
+    ]
     return (
         // <!-- main portfolio -->
         <main className="container" id="main">
@@ -18,159 +75,56 @@ const Projects = () => {
                     <h1>Portfolio</h1>
                 </div>
             </div>
-            {/* <!-- first row of images --> */}
             <div className="row">
-                <article className="col-md-4">
-                    {/* <!-- Tribute page --> */}
-                    <a href="https://elliott-andrew.github.io/tribute-page/" target="_blank" rel="noreferrer">
-                        <img src={TRIBUTE}
-                            className="img-fluid portfolio-images" alt="Cibo Matto - Tribute Page" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Tribute Page</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col  mb-4">
-                            <p>Band tribute page built for bootcamp prework.</p>
-                            <a href="https://elliott-andrew.github.io/tribute-page/">Band Tribute Page</a>
-                        </div>
-                    </div>
-                </article>
-                <article className="col-md-4">
-                    {/* <!-- Horseoin Refactoring --> */}
-                    <a href="https://elliott-andrew.github.io/Code-Refractor/" target="_blank" rel="noreferrer">
-                        <img src={HORSEON}
-                            className="img-fluid portfolio-images" alt="Horiseon - Website Refractor" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Horiseon Refactoring</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col  mb-4">
-                            <p>Refactored a marketing agency website.</p>
-                            <a href="https://elliott-andrew.github.io/Code-Refractor/">Horiseon Refactoring</a>
-                        </div>
-                    </div>
-                </article>
-                <article className="col-md-4">
-                    {/* <!-- Prework Bio page --> */}
-                    <a href="https://elliott-andrew.github.io/prework-about-me/" target="_blank" rel="noreferrer">
-                        <img src={INTRO_PAGE}
-                            className="img-fluid portfolio-images" alt="Andrew Elliott - Bio" />
-                    </a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Introduction Page</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col mb-4">
-                            <p>Introduction page built for bootcamp prework.</p>
-                            <a href="https://elliott-andrew.github.io/prework-about-me/">Introduction Page</a>
-                        </div>
-                    </div>
-                </article>
+                <Project
+                    src={projects[0].src}
+                    name={projects[0].name}
+                    github={projects[0].github}
+                    description={projects[0].description} />
+                <Project
+                    src={projects[1].src}
+                    name={projects[1].name}
+                    github={projects[1].github}
+                    description={projects[1].description} />
+                <Project
+                    src={projects[2].src}
+                    name={projects[2].name}
+                    github={projects[2].github}
+                    description={projects[2].description} />
             </div>
-            {/* <!-- second row of images --> */}
             <div className="row">
-                {/* <!-- Google Homepage --> */}
-                <article className="col-md-4"><a href="https://elliott-andrew.github.io/google-homepage/" target="_blank" rel="noreferrer"><img
-                    src={GOOGLE_HOMEPAGE}
-                    className="img-fluid portfolio-images" alt="Google Homepage Mock Up" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Google Homepage</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col  mb-4">
-                            <p>Rebuild of the Google homepage.</p>
-                            <a href="https://elliott-andrew.github.io/google-homepage/">Google Homepage</a>
-                        </div>
-                    </div>
-                </article>
-                {/* <!-- chopCheese --> */}
-                <article className="col-md-4"><a href="https://github.com/elliott-andrew/Code-Quiz" target="_blank" rel="noreferrer"><img
-                    src={CODEQUIZ} className="img-fluid portfolio-images"
-                    alt="Code Quiz Homepage" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Code Quiz</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col  mb-4">
-                            <p>Code quiz exercise.</p>
-                            <a href="https://github.com/elliott-andrew/Code-Quiz">Code Quiz</a>
-                        </div>
-                    </div>
-                </article>
-                {/* <!-- Code Quiz --> */}
-                <article className="col-md-4"><a href="https://mikevitelli.github.io/chopCheese/" target="_blank" rel="noreferrer"><img
-                    src={CHOPCHEESE} className="img-fluid portfolio-images"
-                    alt="chopCheese Homepage" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>.chopCheese</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col  mb-4">
-                            <p>Content aggregation app.</p>
-                            <a href="https://mikevitelli.github.io/chopCheese/">.chopCheese</a>
-                        </div>
-                    </div>
-                </article>
+                <Project
+                    src={projects[3].src}
+                    name={projects[3].name}
+                    github={projects[3].github}
+                    description={projects[3].description} />
+                <Project
+                    src={projects[4].src}
+                    name={projects[4].name}
+                    github={projects[4].github}
+                    description={projects[4].description} />
+                <Project
+                    src={projects[5].src}
+                    name={projects[5].name}
+                    github={projects[5].github}
+                    description={projects[5].description} />
             </div>
             <div className="row mb-5">
-                {/* <!-- Weather Dashboard --> */}
-                <article className="col-md-4"><a href="https://github.com/elliott-andrew/weather-dashboard" target="_blank" rel="noreferrer"><img
-                    src={WEATHERDASHBOARD} className="img-fluid portfolio-images"
-                    alt="Weather Dashboard Homepage" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Weather Dashboard</h2>
-                        </div>
-                    </div>
-                    <div className="row mb-4">
-                        <div className="col  mb-4">
-                            <p>Weather forecasting app.</p>
-                            <a href="https://github.com/elliott-andrew/weather-dashboard">Weather Dashboard</a>
-                        </div>
-                    </div>
-                </article>
-                <article className="col-md-4"><a href="https://github.com/elliott-andrew/Employee-Tracker" target="_blank" rel="noreferrer"><img
-                    src={EMPLOYEESUMMARY} className="img-fluid portfolio-images"
-                    alt="Weather Dashboard Homepage" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Employee Tracker</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col  mb-4">
-                            <p>Easily organize your employees.</p>
-                            <a href="https://github.com/elliott-andrew/Employee-Tracker">Employee Tracker</a>
-                        </div>
-                    </div>
-                </article>
-                <article className="col-md-4"><a href="https://foodappbcs.herokuapp.com" target="_blank" rel="noreferrer"><img
-                    src={BROWNBAG} className="img-fluid portfolio-images"
-                    alt="Weather Dashboard Homepage" /></a>
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h2>Brown Bag</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col  mb-4">
-                            <p>An online ordering app for local restaurants.</p>
-                            <a href="https://foodappbcs.herokuapp.com">Employee Tracker</a>
-                        </div>
-                    </div>
-                </article>
+                <Project
+                    src={projects[6].src}
+                    name={projects[6].name}
+                    github={projects[6].github}
+                    description={projects[6].description} />
+                <Project
+                    src={projects[7].src}
+                    name={projects[7].name}
+                    github={projects[7].github}
+                    description={projects[7].description} />
+                <Project
+                    src={projects[8].src}
+                    name={projects[8].name}
+                    github={projects[8].github}
+                    description={projects[8].description} />
             </div>
         </main>
     )
