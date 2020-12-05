54f39c5c7b67bcb4dd5a69be8e4c1d6fe1947d1c
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-04 19:01:33 -0500

    Inserted footer HTML and imported it to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar"
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Footer />
     </Router>
   );
 }
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+
+const Footer = () => {
+    return (
+        <footer class="footer">
+            <div class="row">
+                <div class="container">
+                    <h2>Andrew Elliott</h2>
+                </div>
+
+            </div>
+        </footer>
+    )
+};
+
+export default Footer
