b70b49c4a3730218a27127623e35928fe1c649c4
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-04 22:32:19 -0500

    Updated routes for gh pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Resume from "./Pages/Resume";
 
 function App() {
   return (
-    <Router basename='/'>
+    <Router basename='react-portfolio/'>
       <Header />
       <Route exact path="/" component={Index} />
       <Route exact path="/portfolio" component={Portfolio} />
