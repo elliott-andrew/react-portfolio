7e72e6b3010bcda2906435cae96a850c0d2fa136
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-04 19:50:11 -0500

    Moved images and linked profile image

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IMG_3549 from "../images/IMG_3549.png"
 
 const Index = () => {
     return (
@@ -10,7 +11,7 @@ const Index = () => {
             </div>
             <article className="row">
                 <section className="col-md-4">
-                    <img src="/public/images/IMG_3549.png" className= "img-fluid" alt="Andrew Elliott" />
+                    <img src={IMG_3549} alt="Andrew Elliott" className="img-fluid" />
                 </section>
                 <section className="col-md-8" id="about-me-bio">
                     <p id="bio-big">I'm Andrew Elliott. A graphic designer producing <span className="emphasis">brand identities</span> <i className="fas fa-fingerprint"></i>, <span className="emphasis">packaging</span> <i className="fas fa-box-open"></i>, and <span className="emphasis">websites</span> <i className="fas fa-mouse-pointer"></i>.</p> <p>Passionate about design for both print and screen. I have been fortunate to work with a mix of brands, including Brooklyn Roasting Company, Oreo, Chips Ahoy, William Greenberg, and VOSS.</p>
