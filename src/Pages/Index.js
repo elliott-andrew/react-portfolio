2b2e9d960acf6c8619201e0fb760a500471fd923
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-04 19:40:04 -0500

    Updated yarn.lock

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -10,7 +10,7 @@ const Index = () => {
             </div>
             <article className="row">
                 <section className="col-md-4">
-                    <img src="public/images/IMG_3549.png" className= "img-fluid" alt="Andrew Elliott" />
+                    <img src="/public/images/IMG_3549.png" className= "img-fluid" alt="Andrew Elliott" />
                 </section>
                 <section className="col-md-8" id="about-me-bio">
                     <p id="bio-big">I'm Andrew Elliott. A graphic designer producing <span className="emphasis">brand identities</span> <i className="fas fa-fingerprint"></i>, <span className="emphasis">packaging</span> <i className="fas fa-box-open"></i>, and <span className="emphasis">websites</span> <i className="fas fa-mouse-pointer"></i>.</p> <p>Passionate about design for both print and screen. I have been fortunate to work with a mix of brands, including Brooklyn Roasting Company, Oreo, Chips Ahoy, William Greenberg, and VOSS.</p>
