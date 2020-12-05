173404bdad6cc8f181531b0d95a91561d4dc60bc
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-04 19:35:55 -0500

    Imported style.css and images

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar"
+import Index from "./Pages/Index"
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Route exact path="/" component={Index} />
       <Footer />
     </Router>
   );
